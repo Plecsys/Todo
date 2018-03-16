77e72292b46e179315aa487b8089c137e97d1535
Plecsys <crm@plecsys.net>
2018-03-16 11:42:37 -0600

    Summary changed.

--- __manifest__.py
+++ __manifest__.py
@@ -3,8 +3,7 @@
     'name': "Todo",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Todo_app""",
 
     'description': """
         Long description of module's purpose
